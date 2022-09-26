d4b4295d128ee3d7a45eb410da13bdccf8cc8149
Jmeniyi <revoded@mail.ru>
2022-09-26 13:17:39 +0300

    55

--- src/App.vue
+++ src/App.vue
@@ -52,7 +52,7 @@
                 to="/FourthPrj"
                 class="btn btn-outline-secondary text-light"
                 aria-current="page"
-                >Fourth4 Project</router-link
+                >Fourth446 Project</router-link
               >
             </li>
           </ul>
